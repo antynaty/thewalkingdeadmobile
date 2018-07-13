fce3e2b0e8ca0d51604a49f753634a05221a1ee6
antynaty <n.vergara92@gmail.com>
2018-07-13 11:21:44 -0300

    nada en form

--- src/screens/Login/LoginForm.js
+++ src/screens/Login/LoginForm.js
@@ -61,7 +61,7 @@ class LoginForm extends React.Component {
             collection.password=this.state.password
             console.warn(collection);
  
-            const url = 'localhost:3001/user/'
+            const url = '192.168.0.13:3001/user/'
             fetch(url, {
             method: 'POST',
             body: JSON.stringify(collection),
